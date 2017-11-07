ff15d12b1cb9829bba051cbf39166ae5894267e0
Nitin <nitin.patil@werafoods.com>
2017-11-07 12:24:15 +0530

    Changes

--- app/src/main/java/com/example/administrator/mvp/MainActivity.java
+++ app/src/main/java/com/example/administrator/mvp/MainActivity.java
@@ -2,29 +2,46 @@ package com.example.administrator.mvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.administrator.mvp.model.SigninPresenterImpl;
+import com.example.administrator.mvp.model.SigninPresenter_Impl;
 import com.example.administrator.mvp.presenter.SignInPresenter;
+import com.example.administrator.mvp.presenter.Signin_Presenter;
 import com.example.administrator.mvp.view.SigninView;
+import com.example.administrator.mvp.view.Signin_view;
 
-public class MainActivity extends AppCompatActivity implements SigninView {
+public class MainActivity extends AppCompatActivity implements Signin_view {
 
-    private SignInPresenter signInPresenter;
+    private Signin_Presenter signinPresenter_;
+    EditText etUsername, etPassword;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Comments
-        signInPresenter = new SigninPresenterImpl(this);
-        signInPresenter.signIn("ni3", "pass");
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signinPresenter_ = new SigninPresenter_Impl(MainActivity.this);
+                signinPresenter_.signIn( etUsername.getText().toString(), etPassword.getText().toString());
+            }
+        });
+
     }
 
     @Override
     public void signInError() {
-        Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
     }
 
     @Override
